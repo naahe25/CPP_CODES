6951f94a19c7cbbf65572bb1b68c2e6a21c04fe0
naahe25 <naahebd25@gmail.com>
2023-08-17 19:20:26 +0600

    trial

---
+++ Ifelse2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+
+    cin>>N;
+
+
+    if(N>=6){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    
+
+}
