29ac918e0840f7e41fb786a1bd26b646949c64f6
naahe25 <naahebd25@gmail.com>
2023-08-18 02:42:18 +0600

    codes update

--- code2.cpp
+++ code2.cpp
@@ -1,1 +1,23 @@
-#include<bits
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,X,Y,Z;
+
+cin>>T;
+
+while (T--)
+{
+    cin>>X>>Y;
+    if(X>Y){
+    Z=X-Y;
+
+cout<<Z<<endl;
+    }
+    else{
+        cout<<"0"<<endl;
+    }
+}
+
+
+
+}
